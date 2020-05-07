67c8100b39442313eb50126410b6645b417fd371
DungNgoc <you@example.com>
2020-05-07 22:39:32 +0700

    DecisionTrees have not done

---
+++ DesisionTree/DesisionTree/NoCount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesisionTree
+{
+    class NoCount
+    {
+        public int no;
+
+        public int NO
+        {
+
+            get
+            {
+                return no;
+            }
+            set
+            {
+                no = value;
+            }
+        }
+
+        public NoCount()
+        {
+            no = 0;
+        }
+    }
+}
---
+++ DesisionTree/DesisionTree/YesCount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesisionTree
+{
+    class YesCount
+    {
+        public int yes;
+
+        public int YES
+        {
+
+            get
+            {
+                return yes;
+            }
+            set
+            {
+                yes = value;
+            }
+        }
+
+        public YesCount()
+        {
+            yes = 0;
+        }
+    }
+}
